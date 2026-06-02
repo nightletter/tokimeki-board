e0288fb10bb02c80aa5e1adb61c0f3cf8d52c2d1
macmini <nightletter65@gmail.com>
2026-06-02 21:33:25 +0900

    테스트 버전 롤백

--- src/core/config.py
+++ src/core/config.py
@@ -60,7 +60,7 @@ def default_config() -> AppConfig:
     
     return AppConfig(
         app_name="TOKIMEKI BOARD",
-        version="0.0.1",
+        version="0.1.0",
         size=display_sizes.m_size,
         motion=MotionConfig(
             offset_x=10,
